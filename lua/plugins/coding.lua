6a4832aeb948eec49ef13781add79c6c08552167
elshenak <elshenak@gmail.com>
2025-11-16 15:54:13 -0500

    chore: update plugins to latest nvim

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -7,20 +7,7 @@ return {
 	},
 
 	-- Go forward/backward with square brackets
-	{
-		"echasnovski/mini.bracketed",
-		event = "BufReadPost",
-		config = function()
-			local bracketed = require("mini.bracketed")
-			bracketed.setup({
-				file = { suffix = "" },
-				window = { suffix = "" },
-				quickfix = { suffix = "" },
-				yank = { suffix = "" },
-				treesitter = { suffix = "n" },
-			})
-		end,
-	},
+	{ 'nvim-mini/mini.nvim', version = false },
 
 	-- Better increase/descrease
 	{
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,65 +1,41 @@
--- Highlight, edit, and navigate code
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		build = function()
-			pcall(require("nvim-treesitter.install").update({ with_sync = true }))
-		end,
+		lazy = false,
+		branch = "main",
+		build = ":TSUpdate",
 		dependencies = {
 			"nvim-treesitter/nvim-treesitter-textobjects",
 		},
+
 		config = function()
-			require("nvim-treesitter.configs").setup({
-				-- Add languages to be installed here that you want installed for treesitter
-				-- ensure_installed = { "markdown", "c", "cpp", "go", "lua", "javascript", "typescript", "python", "rust", "vimdoc", "vim" },
-				auto_install = true,
-				highlight = { enable = true },
-				indent = { enable = true, disable = { "python" } },
-				incremental_selection = {
-					enable = true,
-					keymaps = {
-						init_selection = "<c-space>",
-						node_incremental = "<c-space>",
-						scope_incremental = "<c-s>",
-						node_decremental = "<c-backspace>",
-					},
-				},
-				textobjects = {
-					select = {
-						enable = true,
-						lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-						keymaps = {
-							-- You can use the capture groups defined in textobjects.scm
-							["aa"] = "@parameter.outer",
-							["ia"] = "@parameter.inner",
-							["af"] = "@function.outer",
-							["if"] = "@function.inner",
-							["ac"] = "@class.outer",
-							["ic"] = "@class.inner",
-						},
-					},
-					move = {
-						enable = true,
-						set_jumps = true, -- whether to set jumps in the jumplist
-						goto_next_start = {
-							["]m"] = "@function.outer",
-							["]]"] = "@class.outer",
-						},
-						goto_next_end = {
-							["]M"] = "@function.outer",
-							["]["] = "@class.outer",
-						},
-						goto_previous_start = {
-							["[m"] = "@function.outer",
-							["[["] = "@class.outer",
-						},
-						goto_previous_end = {
-							["[M"] = "@function.outer",
-							["[]"] = "@class.outer",
-						},
-					},
-				},
+			-- treesitter setup
+			require("nvim-treesitter").setup({
+				install_dir = vim.fn.stdpath("data") .. "/site",
 			})
+
+			-- install parsers asynchronously
+			require("nvim-treesitter").install({
+				"rust",
+				"javascript",
+				"python",
+				"go",
+				"typescript",
+				"c",
+				"zig",
+				"arduino",
+				"cmake",
+				"css",
+				"cpp",
+				"csv",
+				"dockerfile",
+				"gitignore",
+				"terraform",
+				"tsx",
+				"yaml",
+				"zsh",
+			})
+			-- :wait(300000) -- optional: for synchronous bootstrap
 		end,
 	},
 }
