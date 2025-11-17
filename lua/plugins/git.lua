3ffea33bc502f4f12fcf4ca922b1f023d309d5cb
elshenak <elshenak@gmail.com>
2025-11-17 14:42:12 -0500

    chore: setting up git blame

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,3 +1,10 @@
 return {
+	{
+		"FabijanZulj/blame.nvim",
+		lazy = false,
+		config = function()
+			require("blame").setup({})
+		end,
+	},
 	{ "akinsho/git-conflict.nvim", version = "*", config = true },
 }
